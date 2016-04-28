dd8f6486bc36b2467e5594fda6a335bce03ffba2
chad <chadhokama@gmail.com>
2016-04-28 14:08:36 -0700

    making insightly a package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='insightly',
+	  description='thin python wrapper around insightly api',
+	  url='https://github.com/harvestport/insightly-python',
+	  packages=['insightly']
+	  )
