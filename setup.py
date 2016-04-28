5990f91d3b5dd3d66a300ffcd6ce4b105a5afff3
chad <chadhokama@gmail.com>
2016-04-28 15:01:49 -0700

    syncing version, ignore build stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
 setup(name='insightly',
-	  description='thin python wrapper around insightly api',
-	  url='https://github.com/harvestport/insightly-python',
+	  version='2.2',
+	  description='A thin python wrapper around insightly api. This is a fork of insightly-python which not very active',
+	  url='https://github.com/harvestport/hpx-insightly',
 	  packages=['insightly']
 	  )
