8ada76151ef1646defb064b301261dc234e8acb3
chad <chadhokama@gmail.com>
2016-05-03 12:52:52 -0700

    improved pypi install

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hpx-insightly',
-	  version='2.2',
+	  version='2.2.2',
 	  description='A thin python wrapper around insightly api. This is a fork of insightly-python which not very active',
 	  url='https://github.com/harvestport/hpx-insightly',
 	  packages=['insightly']
